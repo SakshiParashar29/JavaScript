3f744a9142fe3cb4856fa4c9808797e832c0591e
Sakshi Kumari <sakshiparashar2910@gmail.com>
2024-12-20 10:47:47 +0000

    DataTypes tutorial

---
+++ 01_basics/DataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // means treat all JS code as newer version
+
+/*
+  Primitive datatypes
+
+  1. Number => approx limit 2^53, if we have to use bigger number then we can use bigInt usually we
+               don't use it.
+
+  2. string => can be writtern in single as well as double quotes
+  3. boolean => true/false
+  4. null => standAlone means empty when we are declaring a variable and assigning it to NULL means it
+  contains nothing.
+
+  5. undefined => when we declare a varibale but doesn't assign any value
+  6. symbol => for showing uniqueness or use as an identifier for avoiding collisions
+ */
+
+  console.log(typeof "sakshi")
+  console.log(typeof null) // object
+  console.log(typeof undefined) // undefined
+
+  /* Interview question : why the type of NULL is undefined */
