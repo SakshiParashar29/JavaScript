5116b88e694bed390f51eccc19b6cceb746397e2
Sakshi Kumari <sakshiparashar2910@gmail.com>
2024-12-27 14:58:11 +0000

    More about Objects

--- 02_basics/Object.js
+++ 02_basics/Object.js
@@ -47,5 +47,42 @@ const obj3 = {...obj1, ...obj2} //...means spread - spread the obj1 and obj2 in 
 
 console.log(obj3)
 
+//Array of Objects
+const users = [
+     {
+        id: 1,
+        email: "abc@gmail.com"
+     },
+     {
+        id: 2,
+        email: "xyz@gmail.com"
+     },
+     {
+        id: 3,
+        email: "123@gmail.com"
+     },
+     {
+        id: 4,
+        email: "!1!@gmail.com"
+     },
+]
+
+console.log(users[0].email)
+
+const JsUser = {
+    name : "Sakshi",
+    // [mySym] : "KeyValue1",
+    age : 19,
+    location : "Jaipur",
+    email : "Sakshi@google.com",
+    isLoggedIn : false,
+    lastLoginDays : ["Monday", "Saturday"]
+}
+console.log(JsUser);
+console.log(Object.keys(JsUser)); // all the keys of JsUser - datatype array
+console.log(Object.values(JsUser)); // all the values of Jsuser - datatype array
+console.log(Object.entries(JsUser));
+
+console.log(JsUser.hasOwnProperty(isLoggedIn)); // return true if present else false;
 
 
