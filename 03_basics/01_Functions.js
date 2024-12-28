6cae030b5f90b081a205a9767001a950129070ff
Sakshi Kumari <sakshiparashar2910@gmail.com>
2024-12-28 03:46:53 +0000

    Object and Array as argument

--- 03_basics/01_Functions.js
+++ 03_basics/01_Functions.js
@@ -45,3 +45,49 @@ function loginUserMessage(username)
 
 console.log(loginUserMessage("Sakshi")) //Sakshi just logged in
 console.log(loginUserMessage()) // if we don't send arguments it show undefined just logged in. to prevent this we will put an if condition in function so that it don't return unwanted values.
+
+
+
+// ... - triple dot is a both restOf operator and spread of operator based on where they are used. In function parameters when triple dot are used then it called restOf operator
+
+/* when we are making e-commerce website then we don't know how many numbers we are getting for calculation so we use restof operator */
+
+function calculateCartPrice(...num1)
+{
+    let totalPrices = 0;
+    for(let i = 0; i  < num1.length; i++)
+    {
+        totalPrices += num1[i];
+    }
+    return totalPrices;
+
+    /* if we do return num1 then it store all the prices in num1 array and return it in array format*/
+}
+/* restof operator store all the prices in an array and we can calculate price by traversin over the array */
+console.log(calculateCartPrice(40, 70, 700, 2000, 40000))
+
+
+// Passin object
+
+const user = {
+    username : "Sakshi",
+    price : 200
+}
+
+function handleObject(anyObject)
+{
+    return console.log(`Username is ${anyObject.username} & price is ${anyObject.price}`);
+}
+
+handleObject(user);
+
+// Array as argument
+
+const myNewArray = [200, 400, 600, 800]
+
+function returnSecondValue(getArray)
+{
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
