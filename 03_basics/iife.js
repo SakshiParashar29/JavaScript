97ba6f365f559d2c7ca0da17efb167c9b1e9a6f9
Sakshi Kumari <sakshiparashar2910@gmail.com>
2024-12-30 14:06:57 +0000

    call stack and execution

--- 03_basics/iife.js
+++ 03_basics/iife.js
@@ -8,3 +8,23 @@
 ( (name) => {
     console.log(`DB connected two ${name}`)
 })('Sakshi')
+
+
+/* JavaScript Execution context
+    
+   JavaScript is considered single-threaded because it executes code in a single thread of execution, meaning it can handle only one task at a time in its main execution stack. 
+
+   An execution context is like a container that holds everything JavaScript needs to run your code. It keeps track of:
+
+   Where the code is running (global or inside a function).
+   What variables and functions are available at that point in the code.
+
+   three types of execution context : 1. Global execution context 2. Function execution context 3. eval execution context
+
+   JS code runs in two phases : 1. Memory Creation phase (only memory allocation occured) 
+   2. Execution phase (all the operations and airthmetic performance happened)
+
+   refered video number 25.
+
+   
+*/
